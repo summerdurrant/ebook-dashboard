e46e8b4f10076620d5f852681e7bda5c8acd1219
Summer Durrant <sdurrant@umw.edu>
2019-02-04 16:44:54 -0500

    Update platform.sql

--- sql/platform.sql
+++ sql/platform.sql
@@ -1,6 +1,5 @@
 CASE "Platform"."Platform" 
 WHEN 'GOLD' THEN 'Gale' 
 WHEN 'Ithaka' THEN 'JSTOR' 
-WHEN 'ScienceDirect licensed content' 
-THEN 'ScienceDirect' 
+WHEN 'ScienceDirect licensed content' THEN 'ScienceDirect' 
 ELSE "Platform"."Platform" END 
